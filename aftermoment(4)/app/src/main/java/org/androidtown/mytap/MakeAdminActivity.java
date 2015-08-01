317ba132f90b2ad8ef218eb0fc3026d681293b01
constantoh <ohconstant@naver.com>
2015-08-01 16:25:05 +0900

    4th

---
+++ aftermoment(4)/app/src/main/java/org/androidtown/mytap/MakeAdminActivity.java
@@ -0,0 +1,15 @@
+package org.androidtown.mytap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by constant on 15. 8. 1..
+ */
+public class MakeAdminActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_layout);
+    }
+}
